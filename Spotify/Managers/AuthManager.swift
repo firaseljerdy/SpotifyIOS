b36faef64ec0f8adb1c60604c144dbf9f3fdc6ba
Firas El-Jerdy <firas.aljerdy@gmail.com>
2021-07-24 14:28:10 +0300

    Added redirecting URI for signup
    Created button and ondidtap listener to redirect

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,6 +10,22 @@ import Foundation
 final class AuthManager{
     static let shared = AuthManager() //singleton
     
+    struct Constants {
+        static let clientID = "723988cb096d496ca5dc1d08cbc95e26"
+        static let clinetSecret = "14b4b710bfa84d6bb77dd1365429ccd6"
+    }
+    
+    public var signInURL: URL? {
+        let base = "https://accounts.spotify.com/authorize"
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
+    
+    
     private init() {}
     
     var isSignedIn: Bool {
